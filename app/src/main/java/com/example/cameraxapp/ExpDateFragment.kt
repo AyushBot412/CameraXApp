889e0a0e13530d3eded97f17e6e08b9bbd9573a3
RoSriv30 <64049464+RoSriv30@users.noreply.github.com>
2024-01-27 15:16:23 -0800

    Updated ExpDateFragment

--- app/src/main/java/com/example/cameraxapp/ExpDateFragment.kt
+++ app/src/main/java/com/example/cameraxapp/ExpDateFragment.kt
@@ -111,8 +111,8 @@ class ExpDateFragment : Fragment() {
 
 
 //            // Initializing the Image Recognition Inferencer
-//            val inferencer = InferenceLocal()
-//            val classification = inferencer.inference(context, imageProxy)
+            val inferencer = InferenceLocal()
+            val classification = inferencer.inference(context, imageProxy)
 
             var date: String
             val image = mediaImage?.let { InputImage.fromMediaImage(it, 0) }
@@ -120,15 +120,15 @@ class ExpDateFragment : Fragment() {
                 recognizer.process(it)
                     .addOnSuccessListener { visionText ->
                         date = processor.processVisionText(visionText, "exp_date")
-                        if (date != "No Date Found") {
+                        if (date != "No Date Found.") {
                             Log.w("Classified Date:", date)
-//                            if (classification == date) { // if image recognition and text recognition are same, then go with text
-//                                displayText.text = date
-//                            } else if (classification.isBlank()) { // if image is blank, then do text
-//                                displayText.text = date
-//                            } else { // if image isn't blank and is different than text, use image
-//                                displayText.text = classification
-//                            }
+                            if (classification == date) { // if image recognition and text recognition are same, then go with text
+                                displayText.text = date
+                            } else if (classification.isBlank()) { // if image is blank, then do text
+                                displayText.text = date
+                            } else { // if image isn't blank and is different than text, use image
+                                displayText.text = classification
+                            }
 
                         }
 
